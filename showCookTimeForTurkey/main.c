e0c42b8c317e07765c2afd9f620d7fc6e360b82c
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-08-09 13:34:53 +0530

    Initial Commit

---
+++ showCookTimeForTurkey/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  showCookTimeForTurkey
+//
+//  Created by Guwanjith Tennekoon on 8/9/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
